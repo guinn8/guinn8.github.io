6b77c53128138d8628569000f8b32db2374b8d69
Gavin Guinn <gavinguinn1@gmail.com>
2020-07-11 22:33:13 -0600

    updated from drive

--- sketch.js
+++ sketch.js
@@ -1,19 +1,49 @@
-SQ = 640
-SIZE = 26
+
+
+SQ = 1280*2
+SIZE = 80
 OFF = 40
 let slider;
+let primes = []
+let button;
+let seq = [];
+
 function setup() {
     createCanvas(SQ, SQ);
-	
-	
-	
-	
-	
 	slider = createSlider(0, 255, 10, 1);
 	slider.position(SQ/2, SQ+10);
 	slider.style('width', '80px');
-
+	slider.hide()
 	
+	
+	input = createInput();
+	input.position(SQ/2, SQ+10);
+	input.hide()
+  
+	button = createButton('submit');
+	button.position(SQ/2, SQ+50);
+	button.mousePressed(runSeq);
+	button.hide()
+	
+	
+	
+	
+	radio = createRadio();
+	radio.option('Show proper factors',1);
+	radio.option('other',2);
+	radio.style('width', '150px');
+	textAlign(CENTER);
+	
+
+	l = 2
+	while(primes.length < SIZE){
+		
+		if(prime(l)){
+			primes.push(l)
+		} 
+		l= l+1
+		
+	}
 	
 }
 
@@ -21,12 +51,38 @@ function setup() {
 
 function draw() {
 	clear();
-	drawGrid();
-	for(let i=0; i<slider.value() ;i++){
-		factorPair(i);
-	}
 	
+	if(radio.value() == 1){
+		drawGrid();
+		input.hide()
+		button.hide()
+		slider.show()
+		for(let i=0; i<slider.value() ;i++){
+			let temp = factorPair(i);
+			temp.forEach(e =>{
+				plot(e)
+				plot(e.reverse())
+			})
+		}
+	
+	}
+	else if(radio.value() == 2){
+		primeGrid()
+		slider.hide()
+		input.show()
+		button.show()
 		
+		
+		if(seq != []){
+			for(e in seq){
+				primePlot(seq[e][0], seq[e][1], seq[e][2], e)
+				console.log(seq[e][2])
+				
+			}
+		}
+		
+		
+	}
 	
 }
 
@@ -47,6 +103,43 @@ function drawGrid(){
 		textSize(12)
 		textAlign(CENTER, BOTTOM)
 		text(i, OFF + (i * inter), SQ-OFF+22) //Dont mind the magic numbres
+		
+	}
+}
+
+function primeGrid(){
+	
+	inter = Math.floor((SQ-OFF)/ SIZE)
+	lineLen = inter * (SIZE-1)
+	i = 0
+	fill(color(0,0,0))
+	for(p in primes){
+		
+		line(OFF, (SQ-OFF) - (i * inter), OFF + lineLen, (SQ-OFF) - (i * inter));
+		textSize(12)
+		textAlign(RIGHT)
+		text(primes[p], OFF -10, (SQ-OFF) - (i * inter) + 3) //Dont mind the magic numbres
+		i++
+	}
+	
+	var top = (SQ-OFF) - ((SIZE-1) * inter)
+	i = 0
+	for(p in primes){
+
+		line(OFF + (i * inter), top, OFF + (i * inter), SQ-OFF);
+		textSize(12)
+		textAlign(CENTER, BOTTOM)
+		text(primes[p], OFF + (i * inter), SQ-OFF+22) //Dont mind the magic numbres
+		i++
+	}
+}
+
+function primePlot(x, y, even, num){
+	if(x<SIZE && y < SIZE){
+		if(even)fill(color(0, 0, 255))
+		else fill(color(255, 0, 0))
+		circle(OFF + (x * inter), (SQ-OFF) - (y * inter), 10)
+		text(num, OFF + (x * inter)-5, (SQ-OFF) - (y * inter)-5)
 	}
 }
 
@@ -64,22 +157,16 @@ function plot(list){
 	
 }
 
-
-
-function factorPair(n){
-	 
-	let max = Math.floor(Math.sqrt(n))
-	let factors = [] 
-	for(i = 2; i <= max; i++){
-		if(n % i == 0){
-			factors.push([i, n/i])
-		}
+function runSeq(){
+	seq = []
+	n = input.value()
+	pair = distinctPrimes(input.value())
+	while(n > 8){
+		if(n %2 ==0) seq.push([primes.indexOf(pair[0]), primes.indexOf(pair[1]), 0])
+		else seq.push([primes.indexOf(pair[0]), primes.indexOf(pair[1]), 1])
+		n =sumDiv(n)
+		pair = distinctPrimes(n)
 	}
+
 	
-	factors.forEach(ele =>{
-		plot(ele);
-		plot(ele.reverse());
-		return true;
-	});
-	//return factors
 }
